fdcbd5a5771ee82b5353c4a2cf8b487de47e67ad
George Radu <radu.george.mihai.21@gmail.com>
2023-04-30 03:34:52 +0300

    add alerts for review broken + grafana dashboard

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ set -x
 mkdir -p prometheus/durable loki/durable
 chmod a+w prometheus/durable loki/durable
 
-DOCKER_DEFAULT_PLATFORM=linux/amd64 docker compose up -d --build
+docker compose up -d --build
